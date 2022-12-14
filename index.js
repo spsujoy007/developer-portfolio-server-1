23c4775656d12cfc3865a7bb16e7fe7dccd5bd93
Sujoy Paul <freelancersujoy@gmail.com>
2022-12-14 11:25:26 +0600

    project by id with filter

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
+require('dotenv').config();
 
 //middleware
 app.use(express.json())
 app.use(cors())
+
+const projects = require ('./projects.json')
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6ke0m0t.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        const projectsCollection = client.db("developer-portfolio1").collection("projects");
+        app.get('/projects', async(req, res) => {
+            const query = {};
+            const result = await projectsCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.get('/project/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id)}
+            const result = await projectsCollection.findOne(query);
+            res.send(result);
+        })
+    }
+    finally{
+        
+    }
+}
+run().catch(err => console.error(err))
 
 
 app.get('/', (req, res) => {
